b3a0a41231ac929f5d60c7a9bb3cb50a4da0c75f
THIRUMANI THEIVARAJAN <misterbell4020@gmail.com>
2022-04-23 15:10:04 +0530

    Logic added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-unit-testing';
+
   sumOfTwoNumbers(a: number, b: number): number {
     return a + b;
   }
 
   ngOnInit() {
-    console.log(this.sumOfTwoNumbers(-10, 20));    
+    console.log(this.sumOfTwoNumbers(10, 20));    
   }
 }
